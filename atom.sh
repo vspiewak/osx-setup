dc8ef956ae5b711370e85ee61b809e890fb9ee1d
Vincent Spiewak <vspiewak@gmail.com>
2019-07-02 01:58:51 +0200

    Add atom.sh script

---
+++ atom.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# disable welcome package
+apm disable welcome
+
+# install some packages
+apm install language-markdown
+apm install language-go
+apm install language-vue
+apm install language-gherkin
+apm install file-icons
--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 CURRENT_DIR=$(dirname $0)
 
 # Ask for the administrator password upfront
+echo "Enter root password"
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
@@ -13,5 +14,7 @@ ${CURRENT_DIR}/update-osx.sh
 ${CURRENT_DIR}/xcode-cli-tools.sh
 
 ${CURRENT_DIR}/brew.sh
+
+${CURRENT_DIR}/atom.sh
 
 ${CURRENT_DIR}/osx.sh
