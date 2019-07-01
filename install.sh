9ec4778fc789c34089ad4f18278101b2aca69e93
Vincent Spiewak <vspiewak@gmail.com>
2019-07-01 23:23:57 +0200

    Big bang commit

--- install.sh
+++ install.sh
@@ -1,4 +1,33 @@
 #!/usr/bin/env bash
 
-./osxprep.sh
-./brew.sh
+{ # this ensures the entire script is downloaded #
+
+osx-setup_do_install() {
+
+  local YELLOW
+  YELLOW='\033[0;33m'
+  local BLUE
+  BLUE='\033[0;34m'
+  local GREY
+  GREY='\033[0;90m'
+  local NC
+  NC='\033[0m'
+
+  echo -e "${GREY}[1/4]${NC} 🚚 Downloading archive"
+  curl -s -L -o /tmp/master.zip https://github.com/vspiewak/osx-setup/archive/master.zip > /dev/null
+
+  echo -e "${GREY}[2/4]${NC} 🚧 Uncompress archive"
+  unzip -qq -o /tmp/master.zip -d /tmp/
+
+  echo -e "${GREY}[3/4]${NC} 🚀 Launch install"
+  /tmp/osx-setup-master/run.sh
+
+  echo -e "${GREY}[4/4]${NC} 🎉 Done"
+
+}
+
+# launch install
+osx-setup_do_install
+
+
+} # this ensures the entire script is downloaded #
---
+++ update-osx.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Install all available updates
+sudo softwareupdate -ia --verbose
+# Install only recommended available updates
+#sudo softwareupdate -ir --verbose
