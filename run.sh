8d486a4d2813ca43f52da360c05726e706702a8a
Vincent Spiewak <vspiewak@gmail.com>
2019-07-01 23:42:10 +0200

    Fix current dir

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+CURRENT_DIR=$(dirname $0)
+
 # Ask for the administrator password upfront
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+${CURRENT_DIR}/update-osx.sh
 
-./update-osx.sh
-
-./xcode-cli-tools.sh
+${CURRENT_DIR}/xcode-cli-tools.sh
