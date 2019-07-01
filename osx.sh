fa5e15a6cbbf51acdc09b63f65a176268c52e9ab
Vincent Spiewak <vspiewak@gmail.com>
2019-07-02 00:58:21 +0200

    Add some comments in osx.sh

--- osx.sh
+++ osx.sh
@@ -1,3 +1,6 @@
+# remove recently used apps from dock
 defaults write com.apple.dock show-recents -bool no
 defaults write com.apple.dock recent-apps -array # intentionally empty
+
+# reboot dock
 killall Dock
