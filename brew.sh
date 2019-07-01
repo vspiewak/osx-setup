05de395f3db20d0c3a95dec0b40fe14ca1648807
Vincent Spiewak <vspiewak@gmail.com>
2019-07-01 22:00:19 +0200

    first commit

---
+++ brew.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# Install more recent versions of some OS X tools.
+brew install vim --override-system-vi
+
+# Install other useful binaries.
+brew install dark-mode
+brew install git
+brew install tree
+
+# Install Heroku
+brew install heroku/brew/heroku
+heroku update
+
+# Core casks
+brew cask install --appdir="~/Applications" iterm2
+brew cask install --appdir="~/Applications" java8
+
+# Development tool casks
+brew cask install --appdir="/Applications" atom
+
+# Misc casks
+brew cask install --appdir="/Applications" firefox
+brew cask install --appdir="/Applications" google-chrome
+brew cask install --appdir="/Applications" skype
+brew cask install --appdir="/Applications" slack
+brew cask install --appdir="/Applications" vlc
+
+# Install Docker
+brew cask install --appdir="/Applications" docker
+
+# Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzip qlimagesize webpquicklook suspicious-package quicklookase qlvideo
+
+# Remove outdated versions from the cellar.
+brew cleanup
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+./osxprep.sh
+./brew.sh
