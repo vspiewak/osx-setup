a4e168876abd8f023ba19837d9190eb22565048d
Vincent Spiewak <vspiewak@gmail.com>
2019-07-02 01:03:43 +0200

    Remove some dock icons with dockutil

--- osx.sh
+++ osx.sh
@@ -2,5 +2,10 @@
 defaults write com.apple.dock show-recents -bool no
 defaults write com.apple.dock recent-apps -array # intentionally empty
 
+# organize dock
+dockutil --remove Siri
+dockutil --remove Launchpad
+dockutil --remove  "Préférences Système"
+
 # reboot dock
 killall Dock
