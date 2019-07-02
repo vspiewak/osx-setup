c994bee1ba969b7b77aea9c1f19c54e5e1c46901
Vincent Spiewak <vspiewak@gmail.com>
2019-07-02 03:59:51 +0200

    Disable hostname setting

--- run.sh
+++ run.sh
@@ -7,11 +7,9 @@ echo "Enter root password"
 sudo -v
 
 # Ask for hostname
-echo -n "Enter hostname: " 
-read CUSTOM_HOSTNAME
-sudo scutil --set ComputerName "$CUSTOM_HOSTNAME"
-sudo scutil --set LocalHostName "$CUSTOM_HOSTNAME"
-sudo scutil --set HostName "$CUSTOM_HOSTNAME"
+#sudo scutil --set ComputerName "$CUSTOM_HOSTNAME"
+#sudo scutil --set LocalHostName "$CUSTOM_HOSTNAME"
+#sudo scutil --set HostName "$CUSTOM_HOSTNAME"
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
