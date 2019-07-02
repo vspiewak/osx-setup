840277648ee0cf54830d60999f4f6696c08f39ce
Vincent Spiewak <vspiewak@gmail.com>
2019-07-02 02:14:29 +0200

    Fix install.sh indentation

--- install.sh
+++ install.sh
@@ -2,34 +2,34 @@
 
 { # this ensures the entire script is downloaded #
 
-osx-setup_do_install() {
+  osx-setup_do_install() {
 
-  local YELLOW
-  YELLOW='\033[0;33m'
-  local BLUE
-  BLUE='\033[0;34m'
-  local GREY
-  GREY='\033[0;90m'
-  local NC
-  NC='\033[0m'
+    local YELLOW
+    YELLOW='\033[0;33m'
+    local BLUE
+    BLUE='\033[0;34m'
+    local GREY
+    GREY='\033[0;90m'
+    local NC
+    NC='\033[0m'
 
-  echo -e "${GREY}[1/4]${NC} 🚚 Downloading archive"
-  curl -s -L -o /tmp/master.zip https://github.com/vspiewak/osx-setup/archive/master.zip > /dev/null
+    echo -e "${GREY}[1/4]${NC} 🚚 Downloading archive"
+    curl -s -L -o /tmp/master.zip https://github.com/vspiewak/osx-setup/archive/master.zip > /dev/null
 
-  echo -e "${GREY}[2/4]${NC} 🚧 Uncompress archive"
-  unzip -qq -o /tmp/master.zip -d /tmp/
+    echo -e "${GREY}[2/4]${NC} 🚧 Uncompress archive"
+    unzip -qq -o /tmp/master.zip -d /tmp/
 
-  echo -e "${GREY}[3/4]${NC} 🚀 Launch install"
-  /tmp/osx-setup-master/run.sh
+    echo -e "${GREY}[3/4]${NC} 🚀 Launch install"
+    /tmp/osx-setup-master/run.sh
 
-  echo -e "${GREY}[4/4]${NC} 🎉 Done"
+    echo -e "${GREY}[4/4]${NC} 🎉 Done"
 
-  echo -e "\n💡 You should restart ;)\n"
+    echo -e "\n💡 You should restart ;)\n"
 
-}
+  }
 
-# launch install
-osx-setup_do_install
+  # launch install
+  osx-setup_do_install
 
 
 } # this ensures the entire script is downloaded #
