403629ed9f7d815150de5f692624ff9335fefd42
Vincent Spiewak <vspiewak@gmail.com>
2019-07-02 18:14:48 +0200

    Fix hostname

--- run.sh
+++ run.sh
@@ -7,8 +7,7 @@ CURRENT_DIR=$(dirname $0)
 osascript -e 'tell application "System Preferences" to quit'
 
 # Ask for hostname
-echo -n 'Hostname: '
-read -p CUSTOM_HOSTNAME
+read -p 'Hostname: ' CUSTOM_HOSTNAME
 
 # Ask for the administrator password upfront
 echo "Enter root password"
