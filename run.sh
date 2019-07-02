680e8e5c9bf2ee10f0056227a4c862943b6fad12
Vincent Spiewak <vspiewak@gmail.com>
2019-07-02 03:58:11 +0200

    Set CUSTOM_HOSTNAME

--- run.sh
+++ run.sh
@@ -7,10 +7,11 @@ echo "Enter root password"
 sudo -v
 
 # Ask for hostname
-read -p "Enter hostname: " hostname
-sudo scutil --set ComputerName "$hostname"
-sudo scutil --set LocalHostName "$hostname"
-sudo scutil --set HostName "$hostname"
+read "Enter hostname: " 
+read CUSTOM_HOSTNAME
+sudo scutil --set ComputerName "$CUSTOM_HOSTNAME"
+sudo scutil --set LocalHostName "$CUSTOM_HOSTNAME"
+sudo scutil --set HostName "$CUSTOM_HOSTNAME"
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
